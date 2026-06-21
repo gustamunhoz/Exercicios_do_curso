65f1c275b02c4fcd5b190ff7076ab4ebe07aee2b
gustamunhoz <gustav1020610@gmail.com>
2026-06-21 20:57:45 -0300

    Add conversion from hours to minutes

---
+++ conversão_horas_em_minutos.py
@@ -0,0 +1,4 @@
+# Conversão
+hora = float (input("Indique o valor do horario que deseja trocar para minutos: "))
+min = hora * 60
+print ("O valor de %sh em minutos equivale a %.2fmin" % (hora,min))
