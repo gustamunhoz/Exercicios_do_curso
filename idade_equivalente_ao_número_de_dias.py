b5fe053602a7cb766be4ae680af3b54849a90a6e
gustamunhoz <gustav1020610@gmail.com>
2026-06-21 20:51:17 -0300

    Add age to days conversion script

---
+++ idade_equivalente_ao_número_de_dias.py
@@ -0,0 +1,7 @@
+# Exercicio 1
+print ("Digite sua idade em anos, meses e dias, para calcular quantos dias de vida tem:")
+ano = int(input("Anos:"))
+mes = int(input("Meses:"))
+dia = int(input("Dias:"))
+dias = (ano * 365) + (mes * 30) + dia
+print (f"Sua idade em dias equilavale a {dias}dias")
