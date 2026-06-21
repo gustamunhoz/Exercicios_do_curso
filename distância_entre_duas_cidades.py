458c9ede87a89567f2ec58a3b3fca59046cbc779
gustamunhoz <gustav1020610@gmail.com>
2026-06-21 20:14:29 -0300

    Add script to calculate average speed between cities

---
+++ distância_entre_duas_cidades.py
@@ -0,0 +1,6 @@
+#distância entre duas cidades
+print("Um carro percorre a uma certa velocidade.")
+cidades= float(input("Digite a distância entre Guarulhos e São Paulo:")) 
+tempo= float(input("Qual o tempo percorrido pelo carro?"))
+velocidade_média= cidades/tempo
+print("A velocidade média do carro é de", velocidade_média,"km/h")
